2f996828d8397a042df0b2c924462a472eec8218
gitdhanshri <2022bit049@sggs.ac.in>
2024-01-28 23:27:12 +0530

    Adding Multipliacation Method

--- calsi/eng_calsi.java
+++ calsi/eng_calsi.java
@@ -4,20 +4,23 @@ class calsi{
  static int add(int a,int b){
 	return a+b;
 	}
-		/*static int sub(int a,int b){
+		static int sub(int a,int b){
 		return a-b;
-		}*/
-			
-		
-			 public static void main(String[] args){
-					 
-				  int a,b;
-				  Scanner obj=new Scanner(System.in);
-				  a=obj.nextInt();
-				  b=obj.nextInt();
-				  add(a,b);
-				  System.out.println(add(a,b));
+		}
+				static int mul(int a,int b){
+			return a*b;
 			}
+			
+			
+				 public static void main(String[] args){
+						 
+					  int a,b;
+					  Scanner obj=new Scanner(System.in);
+					  a=obj.nextInt();
+					  b=obj.nextInt();
+					  mul(a,b);
+					  System.out.println(mul(a,b));
+				}
 		}	
 				 
 
