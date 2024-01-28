dcf0008d31775d186f943947d8756d69e964b076
gitdhanshri <2022bit049@sggs.ac.in>
2024-01-28 22:22:11 +0530

    Adding file for execution of add function

---
+++ calsi/eng_calsi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class calsi{
+ 
+ static int add(int a,int b){
+	return a+b;
+	}
+		
+	 public static void main(String[] args){
+			 
+		  int a,b;
+		  Scanner obj=new Scanner(System.in);
+		  a=obj.nextInt();
+		  b=obj.nextInt();
+		  add(a,b);
+		  System.out.println(  add(a,b));
+				  
+	}
+}	
+				 
+
