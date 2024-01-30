742814bafc0edc5216a2a903dd52e08da8372cd7
gitdhanshri <2022bit049@sggs.ac.in>
2024-01-30 16:27:55 +0530

    Removal of comments

--- calsi/eng_calsi.java
+++ calsi/eng_calsi.java
@@ -1,20 +1,41 @@
 import java.util.Scanner;
 class calsi{
  
- static int add(int a,int b){
+ /*static int add(int a,int b){
 	return a+b;
 	}
-		
-	 public static void main(String[] args){
-			 
-		  int a,b;
-		  Scanner obj=new Scanner(System.in);
-		  a=obj.nextInt();
-		  b=obj.nextInt();
-		  add(a,b);
-		  System.out.println(  add(a,b));
-				  
-	}
-}	
+		static int sub(int a,int b){
+		return a-b;
+		}
+				static int mul(int a,int b){
+			return a*b;
+			}
+				       static int div(int a,int b){
+						 if(b==0){
+						// System.out.print("Can't divide by one\n");
+						 return -1;
+						 }
+						 else{
+						 return a/b;
+						}
+				      } 
+					       
+						       static int square(int a){
+							return a*a;
+							}*/
+								static int cube(int a){
+							return a*a*a;
+							}
+			
+					 public static void main(String[] args){
+							 
+						  int a,b;
+						  Scanner obj=new Scanner(System.in);
+						  a=obj.nextInt();
+						  //b=obj.nextInt();
+						  	cube(a);
+						  System.out.println(cube(a));
+				}
+		}	
 				 
 
