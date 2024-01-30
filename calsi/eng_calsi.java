26ccfeaab04c1c2228178304818afa08b083424e
gitdhanshri <2022bit049@sggs.ac.in>
2024-01-30 16:00:23 +0530

    Adding file which return square

--- calsi/eng_calsi.java
+++ calsi/eng_calsi.java
@@ -9,7 +9,7 @@ class calsi{
 		}
 				static int mul(int a,int b){
 			return a*b;
-			}*/
+			}
 				       static int div(int a,int b){
 						 if(b==0){
 						// System.out.print("Can't divide by one\n");
@@ -19,16 +19,19 @@ class calsi{
 						 return a/b;
 						}
 				      } 
-					       
+					       */
+					       static int square(int a){
+						return a*a;
+						}
 			
 				 public static void main(String[] args){
 						 
 					  int a,b;
 					  Scanner obj=new Scanner(System.in);
 					  a=obj.nextInt();
-					  b=obj.nextInt();
-					  div(a,b);
-					  System.out.println(div(a,b));
+					  //b=obj.nextInt();
+					  	square(a);
+					  System.out.println(square(a));
 				}
 		}	
 				 
